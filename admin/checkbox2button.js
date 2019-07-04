e8dc1601909e8db61aad1601004b0bfe2995b42a
11601798 <38527104+11601798@users.noreply.github.com>
2019-07-04 16:19:04 +0530

    online movie ticket booking using php

---
+++ admin/checkbox2button.js
@@ -0,0 +1,23 @@
+<div class="checkbox checkbox2button">
+  <label>
+    <input type="checkbox" name="checkbox1" value="1"> Checkbox 1
+  </label>
+</div>
+
+<div class="checkbox checkbox2button">
+  <label>
+    <input type="checkbox" name="checkbox2" value="1"> Checkbox 2
+  </label>
+</div>
+
+<div class="checkbox checkbox2button">
+  <label>
+    <input type="radio" name="radio1" value="1"> Radio Button 1
+  </label>
+</div>
+
+<div class="checkbox checkbox2button">
+  <label>
+    <input type="radio" name="radio1" value="2"> Radio Button 2
+  </label>
+</div>
